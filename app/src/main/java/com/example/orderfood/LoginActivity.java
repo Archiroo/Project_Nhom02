7873dd02cc3c32037ced855eb2b7b5582ac30ce7
Archiroo <90332385+Archiroo@users.noreply.github.com>
2022-03-21 16:41:32 +0700

    Xong đăng nhập

--- app/src/main/java/com/example/orderfood/LoginActivity.java
+++ app/src/main/java/com/example/orderfood/LoginActivity.java
@@ -8,12 +8,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
     // Khai báo biến
     TextView textview_Register, textview_forgot;
     EditText phonenumber, password;
-    Button btn_Login;
+    Button Login_btnLogin;
+    DBHelper database;
 
 
     @Override
@@ -23,6 +25,11 @@ public class LoginActivity extends AppCompatActivity {
 //      Gán biến theo id trong giao diện
         textview_Register =findViewById(R.id.textViewSignUp);
         textview_forgot = findViewById(R.id.forgotPassword);
+        phonenumber = findViewById(R.id.input_Login_Phone);
+        password = findViewById(R.id.input_Login_Password);
+        Login_btnLogin = findViewById(R.id.btnlogin);
+        database = new DBHelper(this);
+
 
 //       Chuyển sang giao diện đăng kí
         textview_Register.setOnClickListener(new View.OnClickListener() {
@@ -44,6 +51,33 @@ public class LoginActivity extends AppCompatActivity {
         phonenumber = (EditText) findViewById(R.id.input_Login_Phone);
         password = (EditText) findViewById(R.id.input_Login_Password);
 
+//      Viết sự kiện cho btn login
+        Login_btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String phone = phonenumber.getText().toString();
+                String pass = password.getText().toString();
+
+                if(phone.equals("") || pass.equals(""))
+                {
+                    Toast.makeText(LoginActivity.this, "Bạn chưa nhập tài khoản hoặc mật khẩu.", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Boolean CheckAccount = database.Check_Account(phone, pass);
+                    if(CheckAccount == true)
+                    {
+                        Toast.makeText(LoginActivity.this, "Đăng nhập thành công.", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(intent);
+                    }
+                    else
+                    {
+                        Toast.makeText(LoginActivity.this, "Sai tài khoản hoặc mật khẩu.", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
 
 
     }
